1068a1aef0158efae6d7f36da58b61bc9f193fbb
Raikozu <francescogaglianese93@gmail.com>
2024-04-15 11:41:07 +0200

    Initial commit

---
+++ src/main/java/com/twentyfive/apaapilayer/ApaApiLayerApplication.java
@@ -0,0 +1,13 @@
+package com.twentyfive.apaapilayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApaApiLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApaApiLayerApplication.class, args);
+    }
+
+}
