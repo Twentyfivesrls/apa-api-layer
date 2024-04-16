ebd873d104e8a874068a8ca897adffe007f47124
Raikozu <francescogaglianese93@gmail.com>
2024-04-16 17:11:25 +0200

    -Added ServletInitializer

--- src/main/java/com/twentyfive/apaapilayer/controllers/TestControllers.java
+++ src/main/java/com/twentyfive/apaapilayer/controllers/TestControllers.java
@@ -1,6 +1,7 @@
 package com.twentyfive.apaapilayer.controllers;
 
 import com.twentyfive.apaapilayer.models.CategoryAPA;
+import com.twentyfive.apaapilayer.models.OrderAPA;
 import com.twentyfive.apaapilayer.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ public class TestControllers {
     @GetMapping("/catTest")
     public void categoryTesting (){
         CategoryAPA ctapa =new CategoryAPA();
+        OrderAPA oApa = new OrderAPA();
         ctapa.setName("cazzi");
         ctapa.setEnabled(true);
         categoryRepository.save(ctapa);
