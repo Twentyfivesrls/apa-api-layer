82a7b345d931bb85c357dbc4bb5b37f425a8cdc8
twentyfivesrl <aurelius99w@hotmail.it>
2024-04-24 11:08:54 +0200

    cambiamenti customer controllers

--- src/main/java/com/twentyfive/apaapilayer/controllers/CartController.java
+++ src/main/java/com/twentyfive/apaapilayer/controllers/CartController.java
@@ -109,25 +109,11 @@ public class CartController {
         }
     }
 
-
-
-    @PostMapping("/buy-single/{id}")
-    public ResponseEntity<Boolean> buySingle(@PathVariable String id, @RequestBody BuyInfosDTO buyInfos) {
-        try {
-            boolean result = customerService.buySingleItem(id, buyInfos.getPositions().get(0),buyInfos.getSelectedPickupDateTime());
-            return ResponseEntity.ok(result);
-        } catch (NumberFormatException e) {
-            return ResponseEntity.badRequest().body(false); // In caso di formato non valido
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build(); // Gestisci altre eccezioni in modo generico
-        }
-    }
-
-    @PostMapping("/buy-multiple/{id}")
+    @PostMapping("/buy-from-cart/{id}")
     public ResponseEntity<Boolean> buyMultiple(@PathVariable String id, @RequestBody BuyInfosDTO buyInfos) {
         try {
             // Converti la lista di stringhe in interi
-            boolean result = customerService.buyMultipleItems(id, buyInfos.getPositions(), buyInfos.getSelectedPickupDateTime());
+            boolean result = customerService.buyItems(id, buyInfos.getPositions(), buyInfos.getSelectedPickupDateTime());
             return ResponseEntity.ok(result);
         } catch (NumberFormatException e) {
             return ResponseEntity.badRequest().body(false); // In caso di formato non valido
@@ -136,14 +122,6 @@ public class CartController {
         }
     }
 
-    @PostMapping("/buy-all/{id}")
-    public ResponseEntity<Boolean> buyAll(@PathVariable String id,@RequestBody BuyInfosDTO buyInfos) {
-        try {
-            boolean result = customerService.buyAllItems(id, buyInfos.getSelectedPickupDateTime());
-            return ResponseEntity.ok(result);
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build(); // Gestisci eccezioni in modo generico
-        }
-    }
+
 
 }
