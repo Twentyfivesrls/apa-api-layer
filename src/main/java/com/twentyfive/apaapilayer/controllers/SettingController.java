b49c8d088a29550ad668a91feb301ca3e09db7f6
Raikozu <francescogaglianese93@gmail.com>
2024-05-15 16:09:35 +0200

    -Now we can get the whole settings, we have an Endpoint!

---
+++ src/main/java/com/twentyfive/apaapilayer/controllers/SettingController.java
@@ -0,0 +1,20 @@
+package com.twentyfive.apaapilayer.controllers;
+
+import com.twentyfive.apaapilayer.models.SettingAPA;
+import com.twentyfive.apaapilayer.repositories.SettingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/settings")
+public class SettingController {
+    private final SettingRepository settingRepository;
+    @GetMapping
+    public ResponseEntity<SettingAPA> get(){
+        return ResponseEntity.ok().body(settingRepository.findById("6628cb0ee48d706a10f32bfa").get());
+    }
+}
