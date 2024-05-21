4f890005067714f16660f4450d042b89568a37af
Raikozu <francescogaglianese93@gmail.com>
2024-05-21 11:35:32 +0200

    -Now Trays only customized false

--- src/main/java/com/twentyfive/apaapilayer/repositories/TrayRepository.java
+++ src/main/java/com/twentyfive/apaapilayer/repositories/TrayRepository.java
@@ -1,6 +1,5 @@
 package com.twentyfive.apaapilayer.repositories;
 
-import com.sun.xml.xsom.impl.scd.Step;
 import com.twentyfive.apaapilayer.models.Tray;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface TrayRepository extends MongoRepository<Tray, String> {
-    List<Tray> findAllByCategoryId(String idCategory);
+    List<Tray> findAllByCategoryIdAndCustomizedFalse(String idCategory);
 }
