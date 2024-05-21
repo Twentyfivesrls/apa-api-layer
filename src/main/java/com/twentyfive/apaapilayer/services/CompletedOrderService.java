6bf52a2f44e210874b72f8058be63234dc362b94
twentyfivesrl <aurelius99w@hotmail.it>
2024-05-21 13:29:43 +0200

    aggiunto numero di active orders e di completed orders nel customer DTO per il get by id detailed

--- src/main/java/com/twentyfive/apaapilayer/services/CompletedOrderService.java
+++ src/main/java/com/twentyfive/apaapilayer/services/CompletedOrderService.java
@@ -102,6 +102,7 @@ public class CompletedOrderService {
         dto.setEmail(customer.getEmail()); // Assumi una relazione uno-a-uno con Customer
         dto.setPhoneNumber(customer.getPhoneNumber()); // Assumi che il telefono sia disponibile
         dto.setPickupDateTime(order.getPickupDate().atTime(order.getPickupTime()).toString());
+        dto.setTotalPrice(order.getTotalPrice());
         return dto;
     }
 
