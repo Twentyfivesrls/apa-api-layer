52544ece67a1d92474cdc652d3f1a74a29dd3f97
Raikozu <francescogaglianese93@gmail.com>
2024-05-30 17:30:30 +0200

    activeOrder by default without sort

--- src/main/java/com/twentyfive/apaapilayer/services/ActiveOrderService.java
+++ src/main/java/com/twentyfive/apaapilayer/services/ActiveOrderService.java
@@ -88,9 +88,8 @@ public class ActiveOrderService {
             Pageable pageable= PageRequest.of(page,size,sort);
             return PageUtilities.convertListToPageWithSorting(realOrder,pageable);
         }
-        Sort sort = Sort.by(Sort.Direction.DESC,"formattedPickupDate");
-        Pageable pageable=PageRequest.of(page,size,sort);
-        return PageUtilities.convertListToPageWithSorting(realOrder,pageable);
+        Pageable pageable=PageRequest.of(page,size);
+        return PageUtilities.convertListToPage(realOrder,pageable);
     }
 
     private OrderAPADTO convertToOrderAPADTO(OrderAPA order) {
