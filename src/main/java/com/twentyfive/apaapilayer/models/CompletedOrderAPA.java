596fc359ea4e59f0a4620b2417c6b74b6529716e
Raikozu <francescogaglianese93@gmail.com>
2024-06-28 16:50:06 +0200

    - FOR TEST WE ARE USING OTHER DOCUMENTS IN CHANGE OF ORDER AND COMPLETEDORDER

--- src/main/java/com/twentyfive/apaapilayer/models/CompletedOrderAPA.java
+++ src/main/java/com/twentyfive/apaapilayer/models/CompletedOrderAPA.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import twentyfive.twentyfiveadapter.generic.ecommerce.models.persistent.CompletedOrder;
 
 
-@Document("completedOrders")
+@Document("testcompletedOrders")
 public class CompletedOrderAPA extends CompletedOrder {
 }
