12e1f30918101ae991d513b8d9bd07861ba21384
Raikozu <francescogaglianese93@gmail.com>
2024-09-26 16:58:47 +0200

    -Refactored all Authorizing every endpoint.

--- src/main/java/com/twentyfive/apaapilayer/controllers/CartController.java
+++ src/main/java/com/twentyfive/apaapilayer/controllers/CartController.java
@@ -22,7 +22,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/cart")
 
-@PreAuthorize("hasRole('ROLE_customer')")
+@PreAuthorize("hasRole('ROLE_customer') or hasRoles('ROLE_admin')")
 public class CartController {
 
     private final CustomerService customerService;
