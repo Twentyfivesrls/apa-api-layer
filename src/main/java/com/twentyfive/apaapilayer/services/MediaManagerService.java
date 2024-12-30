95401e8d29afff291f27878a87f88519c4e22fcb
Raikozu <francescogaglianese93@gmail.com>
2024-12-30 17:24:18 +0100

    -Fix basePath per l'url

--- src/main/java/com/twentyfive/apaapilayer/services/MediaManagerService.java
+++ src/main/java/com/twentyfive/apaapilayer/services/MediaManagerService.java
@@ -45,7 +45,7 @@ public class MediaManagerService {
     public String getPath(HttpServletRequest request) {
         String fullPath = request.getRequestURI();
         String contextPath = request.getContextPath();
-        String basePath = contextPath + "/download/";
+        String basePath = contextPath + "/media/download/";
         return fullPath.substring(basePath.length()); // Estrai il path relativo
     }
 }
